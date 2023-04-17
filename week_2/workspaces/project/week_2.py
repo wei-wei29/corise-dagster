75b500fd5b7aa31c40e3218458448e464de01398
Wei Wei <wei.wei@discordapp.com>
2023-04-17 02:08:55 +0000

    week2 project

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -16,29 +16,67 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(
+    out={"stocks": Out(dagster_type=List[Stock])},
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"},
+    tags={"kind": "s3"},
+    description="Get a list of stocl data from S3"
+)
+def get_s3_data(context):
+    s3_key = context.op_config["s3_key"]
+    stocks = context.resources.s3.get_data(s3_key)
+    # put s3 data into list of Stock
+    stock_list = []
+    for stock in stocks:
+        stock_list.append(Stock.from_list(stock))
+    return stock_list
+
+@op(
+    ins={"stocks": In(dagster_type=List[Stock], description="A list of Stock data")},
+    out={"highest_stock": Out(dagster_type=Aggregation, description="The highest high of stock data")},
+    description="Get the stock with the greatest high value"
+)
+def process_data_op(stocks):
+    highest_stock = max(stocks, key = lambda s: s.high)
+    #print(highest_stock)
+    return Aggregation(date = highest_stock.date, high = highest_stock.high)
+
+@op(
+    ins={"data": In(dagster_type=Aggregation, description="Aggregated stock data")},
+    config_schema={"redis_key": String},
+    required_resource_keys={"redis"},
+    tags={"kind": "redis"},
+    description="Upload an Aggregation to Redis"
+    )
+def put_redis_data(context, aggregation):
+    # convert result to string
+    result_date = str(aggregation.date)
+    result_high = str(aggregation.high)
+    #put data to Redis
+    context.resources.redis.put_data(result_date, result_high)
 
 
-@op
-def process_data():
-    pass
-
-
-@op
-def put_redis_data():
-    pass
-
-
-@op
-def put_s3_data():
-    pass
+@op(
+    ins={"data": In(dagster_type=Aggregation, description="Aggregated stock data")},
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"},
+    tags={"kind": "s3"},
+    description="Upload an Aggregation to S3"
+    )
+def put_s3_data(context, aggregation):
+    # convert result to string
+    result_date = str(aggregation.date)
+    result_high = str(aggregation.high)
+    #put data to Redis
+    context.resources.s3.put_data(result_date, result_high)
 
 
 @graph
 def machine_learning_graph():
-    pass
+    highest_stock = process_data_op(get_s3_data())
+    put_redis_data(highest_stock)
+    put_redis_data(highest_stock)
 
 
 local = {
@@ -55,8 +93,12 @@ docker = {
 
 machine_learning_job_local = machine_learning_graph.to_job(
     name="machine_learning_job_local",
+    config=local,
+    resource_defs={"s3": mock_s3_resource, "redis": redis_resource}
 )
 
 machine_learning_job_docker = machine_learning_graph.to_job(
     name="machine_learning_job_docker",
+    config=docker,
+    resource_defs={"s3": s3_resource, "redis": redis_resource}
 )
